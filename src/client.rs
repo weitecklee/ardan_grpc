cfda6ccfe16114711f374a61438c25fc8620b2c9
arkteck <arkteck@gmail.com>
2026-03-04 14:11:35 -0500

    set up client

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,21 @@
-fn main() {}
+pub mod hello_world {
+    tonic::include_proto!("hello");
+}
+
+use hello_world::HelloRequest;
+use hello_world::greeter_client::GreeterClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
